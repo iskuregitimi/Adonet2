08df220072c00933f1595764b465c8c5c9961b17
Mehmet Fatih AKAN <akanmf@yahoo.com>
2019-04-05 10:56:49 +0300

    İlk chekin

---
+++ AdoNet2/DataAccessLayer/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DataContext
+    {
+        public static DataTable GetPersons(string name)
+        {
+            //TODO: ismini verdiğim adamları bulsun.İsim boş ise hepsini getirsin. 
+            return null;
+        }
+    }
+}
---
+++ AdoNet2/PersonList/Form1.cs
@@ -0,0 +1,27 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = DataContext.GetPersons("");
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
